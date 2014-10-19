71b27103be64553febea8b58b151c4e6140e851b
ikaz <icastillejos@gmail.com>
2014-10-19 15:11:03 -0700

    Working on ex6

--- Exercises/stream-adventure/program6.js
+++ Exercises/stream-adventure/program6.js
@@ -1,17 +1,23 @@
 var concat = require('concat-stream');
-var http = require('http');
+var through = require('through');
 
+
+var writeconcat = concat(function(data){});
+
+
+process.stdin.pipe(writeconcat).pipe(process.stdout);
+
+/*
 var server = http.createServer(
 	function (req, res) {
 		if (req.method == 'POST'){
 			req.pipe(concat(function(body){
 				var obj = JSON.parse(body);
-				res.end(Object.keys(obj).join('\n'));å
-
-
+				res.end(Object.keys(obj).join('\n'));
 			}));
-		}
+		} 
+		else res.end();
 	}
 );
 
-server.listen(5000);
+server.listen(5000); */
